1b7b9604cbae13911485198f70f1d9cd780df345
Starodubov Anton <sapience@yandex.ru>
2018-11-28 21:18:30 +0300

    28.11

---
+++ src/BagOfTokens.java
@@ -0,0 +1,45 @@
+//https://leetcode.com/problems/bag-of-tokens/
+import java.util.Arrays;
+public class BagOfTokens {
+    public static int bagOfTokensScore(int[] tokens, int P) {
+
+        if (tokens.length == 0) return 0;
+    	if (tokens.length == 1 && tokens[0] < P) return 1;
+    	
+    	Arrays.sort(tokens);
+    	boolean changed = false;
+    	int points = 0;
+    	int lowest = tokens[0];
+    	if (P < lowest) return 0;
+    	P -= lowest; points++;
+		int highestPointer = tokens.length - 1;
+		int lowestPointer = 1;
+		
+ 	
+    	while(!changed){
+    		if (tokens[lowestPointer] > P && points == 0) break;
+    		if (tokens[lowestPointer] <= P) {
+    			points++;
+    			P -= tokens[lowestPointer];
+    			lowestPointer++;
+    			continue;
+    		}
+    		if (tokens[lowestPointer] > P && points > 0 && highestPointer - lowestPointer >= 2) {
+    			P += tokens[highestPointer];
+    			highestPointer--;
+    			points--;
+    			continue;
+    		} 
+    		changed = !changed;
+    	}  
+    	
+        return points;
+    }	
+    
+    public static void main(String[] args) {
+    	int[]A = {42,10,26,22};
+    			
+
+    	System.out.println(bagOfTokensScore(A, 	65));
+    }
+}
---
+++ src/FlippingAnImage.java
@@ -0,0 +1,38 @@
+//https://leetcode.com/problems/flipping-an-image/
+
+public class FlippingAnImage {
+    public static int invert(int bit)
+    {
+        return (bit==0) ? 1 : 0;
+    }
+    public static int[][] flipAndInvertImage(int[][] A) {
+        for(int i = 0; i < A.length; i++) {
+            for(int j = 0; j < ((A[i].length + 1)/2); j++) {
+            	int tmp = A[i][A[i].length - j - 1];
+            	A[i][A[i].length - j - 1] = invert(A[i][j]);
+            	A[i][j] = invert(tmp);
+            }
+        }
+        return A;
+    }
+    
+    public static void main(String[] args) {
+    	int[][] A = {{1,1,0,1},{1,0,0,1},{0,0,0,0},{1,1,1,1}};
+        for(int i = 0; i < A.length; i++) {
+            for(int j = 0; j < A[i].length; j++) {
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+        System.out.println("-----------");
+    	A = flipAndInvertImage(A);
+        for(int i = 0; i < A.length; i++) {
+            for(int j = 0; j < A[i].length; j++) {
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+    	
+    }
+    
+}
