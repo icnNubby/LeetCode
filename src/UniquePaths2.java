dce48a1ff3ee9eac950222da8be4305b22efa08b
Starodubov Anton <sapience@yandex.ru>
2019-01-04 21:55:28 +0300

    04.01.2019 refactor

--- src/UniquePaths2.java
+++ src/UniquePaths2.java
@@ -18,26 +18,25 @@ public class UniquePaths2 {
         if (obstacleGrid[0][0] == 1) return 0;
         else obstacleGrid[0][0] = 1;
         for (int i = 1; i < obstacleGrid.length; i++) {
-            if (obstacleGrid[i][0] == 1) {
+            if (obstacleGrid[i][0] == 1)
                 obstacleGrid[i][0] = 0;
-            } else
+            else
                 obstacleGrid[i][0] = obstacleGrid[i - 1][0];
 
         }
         for (int i = 1; i < obstacleGrid[0].length; i++) {
-            if (obstacleGrid[0][i] == 1) {
+            if (obstacleGrid[0][i] == 1)
                 obstacleGrid[0][i] = 0;
-            } else
+            else
                 obstacleGrid[0][i] = obstacleGrid[0][i - 1];
 
         }
         for (int i = 1; i < obstacleGrid.length; i++) {
             for (int j = 1; j < obstacleGrid[i].length; j++) {
-                if (obstacleGrid[i][j] == 1) {
+                if (obstacleGrid[i][j] == 1)
                     obstacleGrid[i][j] = 0;
-                } else {
+                else
                     obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
-                }
             }
         }
         for (int[] line : obstacleGrid) System.out.println(Arrays.toString(line)); // for debug purposes only
